c4e1709d2f1d2dd6a177642c352f3a90c5933376
LUCHER4321 <lucher4321@gmail.com>
2025-03-16 16:05:49 -0300

    Update Table.tsx

--- src/scripts/Table.tsx
+++ src/scripts/Table.tsx
@@ -1,95 +1,52 @@
 import { changeBase } from "@lucher/change-base";
 import "../App.css";
 
-class Queen {
-  x: number;
-  y: number = -1;
-  table: Table;
-  previous?: Queen;
-  next?: Queen;
-
-  static letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-  constructor(x: number, table: Table) {
-    this.x = x;
-    this.table = table;
-  }
-
-  setSides(p: Queen | undefined, n: Queen | undefined){
-    this.previous = p;
-    this.next = n;
-  }
-
-  getPrevious(): Queen[] {
-    if (!this.previous) {
-      return [];
-    }
-    return [this.previous].concat(this.previous.getPrevious());
-  }
-
-  attackingQueen(q: Queen) {
-    return this.y === q.y || Math.abs(this.x - q.x) === Math.abs(this.y - q.y);
-  }
-
-  validPosition() {
-    if(this.y < 0) return false;
-    const prevQueens = this.getPrevious();
-    if (prevQueens.length === 0) {
-      return true;
-    }
-    return !prevQueens
-      .map((q) => this.attackingQueen(q))
-      .reduce((p, c) => p || c);
-  }
-
-  letter() {
-    return changeBase(this.x.toFixed(0).split('').map(s => Number(s)), 10, 26).map((n, index, array) => Queen.letters[n - (index === 0 && array.length > 1 ? 1 : 0)]).join("");
-  }
-
-  position(n: number | undefined = undefined){
-    return this.letter() + (n === undefined ? this.y + 1 : n).toFixed(0);
-  }
-
-  async move(start = false): Promise<string[]> {
-    this.table.currentQueen = this;
-    this.y += start ? 0 : 1;
-    if (this.y >= this.table.n) {
-      this.y = -1;
-      if (this.previous) {
-        return await this.previous.move();
-      }
-      this.table.currentQueen = this;
-      for(const q of this.table.queens){
-        q.y = -1;
-      }
-      return [];
-    }
-    if (!this.validPosition()) {
-      return await this.move();
-    }
-    if (this.next) {
-      return await this.next.move();
-    }
-    return this.table.queens.map((q) => q.position());
-  }
-}
-
 export class Table {
   n: number;
-  queens: Queen[];
-  currentQueen: Queen;
+  queens: number[];
+  currentQueen: number = 0;
 
   constructor(n: number) {
     this.n = n;
-    const queens: Queen[] = []
-    for(let i = 0; i < n; i++){
-      queens.push(new Queen(i, this as Table));
+    this.queens = [...Array(n).keys()].map(() => -1);
+  }
+
+  static letters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  static letter(q: number){
+    return changeBase(q.toFixed(0).split('').map(s => Number(s)), 10, 26).map((n, index, array) => Table.letters[n - (index === 0 && array.length > 1 ? 1 : 0)]).join("");
+  }
+
+  position(q: number, n: number | undefined = undefined){
+    return Table.letter(q) + (n === undefined ? this.queens[q] + 1 : n).toFixed(0);
+  }
+
+  move(update = () => {}){
+    while(this.currentQueen < this.n){
+      if(this.currentQueen < 0) {
+        this.currentQueen = 0;
+        return [];
+      }
+      this.queens[this.currentQueen]++;
+      update();
+      if(this.queens[this.currentQueen] >= this.n){
+        this.queens[this.currentQueen] = -1;
+        update();
+        this.currentQueen--;
+        continue;
+      }
+      let c = false;
+      for(let i = 0; i < this.currentQueen; i++){
+        if(this.queens[i] === this.queens[this.currentQueen] || Math.abs(this.queens[i] - this.queens[this.currentQueen]) === Math.abs(i - this.currentQueen)){
+          c = true;
+          break;
+        }
+      }
+      if(c) continue;
+      this.currentQueen++;
     }
-    for (const [index, item] of queens.entries()) {
-      item.setSides(index > 0 ? queens[index - 1] : undefined, index < n - 1 ? queens[index + 1] : undefined);
-    }
-    this.queens = queens;
-    this.currentQueen = queens[0];
+    this.currentQueen--;
+    return this.queens.map((_, index) => this.position(index));
   }
 
   static icon = ({size}: {size: number}) => <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_tile_ql.svg/1200px-Chess_tile_ql.svg.png" className={`max-w-[${size}px] max-h-[${size}px]`}/>
@@ -99,29 +56,29 @@ export class Table {
       <table>
         <tbody>
           <tr>
-            <td className="bg-red-500"></td>
-            {table.queens.map((item, index) => (
-              <td className="bg-red-500" style={{maxWidth: size, maxHeight: size, minWidth: size, minHeight: size}} key={index}>{item.letter()}</td>
+            <td></td>
+            {table.queens.map((_, index) => (
+              <td key={index}>{Table.letter(index)}</td>
             ))}
-            <td className="bg-red-500"></td>
+            <td></td>
           </tr>
           {table.queens.map((_, index) => (
             <tr key={index}>
-              <td className="bg-red-500" style={{maxWidth: size, maxHeight: size, minWidth: size, minHeight: size}}>{table.n - index}</td>
+              <td>{table.n - index}</td>
               {table.queens.map((item, index1) => (
-                <td className={`${(item.y === table.n - index - 1 && !item.validPosition()) ? "bg-red-400" : ((index + index1) % 2 === 0 ? "bg-white text-black" : "bg-black text-white")}`} style={{maxWidth: size, maxHeight: size, minWidth: size, minHeight: size}} key={index1}>
-                  {(item.y === table.n - index - 1) && <Table.icon size={size}/>}
+                <td className={`${((index + index1) % 2 === 0 ? "bg-white" : "bg-black")}`} style={{maxWidth: size, maxHeight: size, minWidth: size, minHeight: size}} key={index1}>
+                  {(item === table.n - index - 1) && <Table.icon size={size}/>}
                 </td>
               ))}
-              <td className="bg-red-500" style={{maxWidth: size, maxHeight: size, minWidth: size, minHeight: size}}>{table.n - index}</td>
+              <td>{table.n - index}</td>
             </tr>
           ))}
           <tr>
-            <td className="bg-red-500"></td>
-            {table.queens.map((item, index) => (
-              <td className="bg-red-500" style={{maxWidth: size, maxHeight: size, minWidth: size, minHeight: size}} key={index}>{item.letter()}</td>
+            <td></td>
+            {table.queens.map((_, index) => (
+              <td key={index}>{Table.letter(index)}</td>
             ))}
-            <td className="bg-red-500"></td>
+            <td></td>
           </tr>
         </tbody>
       </table>
